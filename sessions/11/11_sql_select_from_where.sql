790324e29d68524f354451b03fee60c6c9d0653f
Tyler Rinker <trinker@campuslabs.com>
2018-04-18 16:13:30 -0400

    next version

--- sessions/11/11_sql_select_from_where.sql
+++
@@ -1,8 +0,0 @@
---##========================================
---## select, from, where & exporting
---##========================================
---## In this demonstration we'll be learning how to do basic SQL quering and 
---## exporting the results.
-
-
-
